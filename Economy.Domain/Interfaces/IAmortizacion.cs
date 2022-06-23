9a118f73100b4f7067ab653040edecb5cbc532b8
josebaltodano <jose.baltodano935@gmail.com>
2022-06-23 13:31:50 -0600

    Adding amortizacion

---
+++ Economy.Domain/Interfaces/IAmortizacion.cs
@@ -0,0 +1,16 @@
+﻿using Economy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Economy.Domain.Interfaces
+{
+   public interface IAmortizacion : IRepository<Amotizacion>
+    {
+        void Metodo1(Amotizacion amotizacion );
+        void Metodo2(Amotizacion amotizacion);
+
+    }
+}
